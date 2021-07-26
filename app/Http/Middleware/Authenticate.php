28a50c125539d87c2ff46bfa132828f9bc696baf
haithemmihoubi <haithemmihoubi1234@gmaail.com>
2021-07-26 04:55:43 +0100

    redirect user when authenticated based on role

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,19 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
+
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            if (Auth::user()->admin) {
+                return '/admin';
+            }
+            return '/home';
+
         }
     }
 }
